f38bf64cd151bcdf608031a5b40d51bb0581660e
Robb Lovell <robbl@moj.io>
2014-11-25 14:53:17 -0800

    Fixes for Displaying results.

--- src/myFirstMojioApp.js
+++ src/myFirstMojioApp.js
@@ -35,47 +35,48 @@
         return mojio_client.authorize(config.redirect_uri);
       } else {
         alert("Authorization Successful.");
-        return mojio_client.token(function(error, result) {
-          mojio_client.get(mojio_client.model("User"), {
-            id: result.UserId
-          }, function(error, result) {
-            var message;
-            message = 'Viewing the location of <strong>';
-            if (result.FirstName) {
-              message += result.FirstName;
-            } else if (result.UserName) {
-              message += result.UserName;
-            } else if (result.LastName) {
-              message += result.LastName;
-            } else if (result.Email) {
-              message += result.Email;
-            } else {
-              message += "Unknown";
-            }
-            message += '</strong>';
-            div = $("#welcome");
-            return div.html(message);
-          });
-          return mojio_client.get(mojio_client.model("Vehicle"), {}, function(error, result) {
-            var i, lat, lng;
-            lat = [];
-            lng = [];
-            i = 0;
-            $.each(result.Data, function(key, value) {
-              if ((value.LastLocation != null) && (value.LastLocation.Lat != null) && (value.LastLocation.Lng != null)) {
-                lat[i] = value.LastLocation.Lat;
-                lng[i] = value.LastLocation.Lng;
-                return i++;
-              }
-            });
-            div = $("#result");
-            if (lat.length > 0) {
-              div.html('The vehicle is at: ' + lat[0] + ", " + lng[0]);
-              return buildMojioMap(lat[0], lng[0]);
-            } else {
-              return div.html("No vehicle detected!");
+        div = $("#welcome");
+        div.html('Authorization Result:<br />');
+        div.append(JSON.stringify(result));
+        mojio_client.get(mojio_client.model("User"), {
+          id: result.UserId
+        }, function(error, result) {
+          var message;
+          message = '<br/><br/>Viewing the location of <strong>';
+          if (result.FirstName) {
+            message += result.FirstName;
+          } else if (result.UserName) {
+            message += result.UserName;
+          } else if (result.LastName) {
+            message += result.LastName;
+          } else if (result.Email) {
+            message += result.Email;
+          } else {
+            message += "Unknown";
+          }
+          message += '</strong>';
+          div = $("#welcome");
+          return div.append(message);
+        });
+        return mojio_client.get(mojio_client.model("Vehicle"), {}, function(error, result) {
+          var i, lat, lng;
+          lat = [];
+          lng = [];
+          i = 0;
+          $.each(result.Data, function(key, value) {
+            if ((value.LastLocation != null) && (value.LastLocation.Lat != null) && (value.LastLocation.Lng != null)) {
+              lat[i] = value.LastLocation.Lat;
+              lng[i] = value.LastLocation.Lng;
+              return i++;
             }
           });
+          div = $("#result");
+          if (lat.length > 0) {
+            div.html('The vehicle is at: ' + lat[0] + ", " + lng[0]);
+            return buildMojioMap(lat[0], lng[0]);
+          } else {
+            return div.html("No vehicle detected!");
+          }
         });
       }
     });
